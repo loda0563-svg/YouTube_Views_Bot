5b235f46b51d045b8167c749414cfd877379a549
loda0563-svg <loda0563@gmail.com>
2026-08-27 19:28:35 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -16,7 +16,7 @@ printf "\n \e[1;92mUpdating \e[1;94mYouTube_Views_Bot\e[1;92m ...\n\n"
 sleep 1.5
 cd ..
 rm -rf YouTube_Views_Bot
-git clone https://github.com/AbirHasan2005/YouTube_Views_Bot
+git clone https://github.com/loda0563-svg/YouTube_Views_Bot
 cd YouTube_Views_Bot
 
 }
